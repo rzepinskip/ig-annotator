6792cbeab4656b53c49715a915ffb96478789df7
Paweł Rzepiński <rzepinski.pawel@gmail.com>
2019-11-17 14:21:28 +0100

    Fix missing model error

--- stanford.py
+++ stanford.py
@@ -1,5 +1,8 @@
 import pandas as pd
 from igannotator.annotator import StanfordAnnotator
+import stanfordnlp
+
+stanfordnlp.download("pl", confirm_if_exists=False, force=True)
 
 statute_df = pd.read_csv("data/RegulaminSejmuRzeczypospolitej.csv")
 example = statute_df.iloc[16].Content
