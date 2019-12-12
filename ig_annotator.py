e897d1178eef2b2cbe128783b1feb3c0b7d48abf
Paweł Rzepiński <rzepinski.pawel@gmail.com>
2019-12-12 16:21:29 +0100

    Clean up

--- ig_annotator.py
+++ ig_annotator.py
@@ -27,6 +27,39 @@ def annotate_file(file_path):
     return root
 
 
+def write_output_to_mae(output_file, tree, tags):
+    with open(output_file, "w") as output:
+        output.write('<?xml version="1.0" encoding="UTF-8" ?>\n')
+        output.write("<ADICO_test_1>\n")
+
+        sentence = ""
+        for x in tree.get_all_descendants():
+            sentence += " " + x.value if x.tag != "PUNCT" and x.id != 1 else x.value
+        output.write(f"<TEXT><![CDATA[{sentence}]]></TEXT>\n")
+
+        output.write("<TAGS>\n")
+        id = 0
+        tag_names = {
+            "IGElement.AIM": "aIm",
+            "IGElement.ATTRIBUTE": "Attribute",
+            "IGElement.OBJECT": "oBject",
+            "IGElement.SEPARATOR": "SEPARATOR",
+            "IGElement.DEONTIC": "Deontic",
+        }
+        for tag in tags:
+            words = [x[1] for x in sorted(tag.words, key=lambda x: x[0])]
+            start = sentence.find(words[0])
+            stop = sentence.find(words[-1]) + len(words[-1])
+            spans = f"{start}~{stop}"
+            output.write(
+                f'<{tag_names[str(tag.tag_name)]} id="x{id}" spans="{spans}" text="{sentence[start:stop]}" />\n'
+            )
+            id += 1
+        output.write("</TAGS>\n")
+
+        output.write("</ADICO_test_1>")
+
+
 @click.command()
 @click.argument("directory", type=click.Path(exists=True))
 def annotate_files(directory):
@@ -34,8 +67,6 @@ def annotate_files(directory):
 
     for input in listdir(directory):
         file_path = pathlib.Path(directory) / pathlib.Path(input)
-        # if input != "stanford1.conllu":
-        #     continue
         if file_path.is_file() and file_path.suffix == ".conllu":
             with open(file_path, "r+") as input:
                 df = pd.read_csv(input, sep="\t", header=None)
@@ -46,38 +77,7 @@ def annotate_files(directory):
             for tag in tags:
                 print(f"{tag.tag_name}: {tag.words}")
 
-            with open(file_path.with_suffix(".xml"), "w") as output:
-                output.write('<?xml version="1.0" encoding="UTF-8" ?>\n')
-                output.write("<ADICO_test_1>\n")
-
-                sentence = ""
-                for x in tree.get_all_descendants():
-                    sentence += (
-                        " " + x.value if x.tag != "PUNCT" and x.id != 1 else x.value
-                    )
-                output.write(f"<TEXT><![CDATA[{sentence}]]></TEXT>\n")
-
-                output.write("<TAGS>\n")
-                id = 0
-                tag_names = {
-                    "IGElement.AIM": "aIm",
-                    "IGElement.ATTRIBUTE": "Attribute",
-                    "IGElement.OBJECT": "oBject",
-                    "IGElement.SEPARATOR": "SEPARATOR",
-                    "IGElement.DEONTIC": "Deontic",
-                }
-                for tag in tags:
-                    words = [x[1] for x in sorted(tag.words, key=lambda x: x[0])]
-                    start = sentence.find(words[0])
-                    stop = sentence.find(words[-1]) + len(words[-1])
-                    spans = f"{start}~{stop}"
-                    output.write(
-                        f'<{tag_names[str(tag.tag_name)]} id="x{id}" spans="{spans}" text="{sentence[start:stop]}" />\n'
-                    )
-                    id += 1
-                output.write("</TAGS>\n")
-
-                output.write("</ADICO_test_1>")
+            write_output_to_mae(file_path.with_suffix(".xml"), tree, tags)
 
 
 if __name__ == "__main__":
