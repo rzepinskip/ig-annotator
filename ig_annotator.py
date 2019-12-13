1a7735f3d946e91c3d3fc9468b852a6ee9f95e4a
Paweł Rzepiński <rzepinski.pawel@gmail.com>
2019-12-13 12:29:50 +0100

    Use one MAE file

--- ig_annotator.py
+++ ig_annotator.py
@@ -27,41 +27,63 @@ def annotate_file(file_path):
     return root
 
 
-def write_output_to_mae(output_file, tree, tags):
-    with open(output_file, "w", encoding='utf-8') as output:
+def get_sentence_and_tags(tree, tags):
+    sentence = ""
+    id_to_position = dict()
+    for x in tree.get_all_descendants():
+        if x.tag != "PUNCT" and x.id != 1:
+            id_to_position[x.id] = len(sentence) + 1
+            sentence += " " + x.value
+        else:
+            id_to_position[x.id] = len(sentence)
+            sentence += x.value
+
+    tag_names = {
+        "IGElement.AIM": "aIm",
+        "IGElement.ATTRIBUTE": "Attribute",
+        "IGElement.OBJECT": "oBject",
+        "IGElement.ACTOR": "aCtor",
+        "IGElement.SEPARATOR": "SEPARATOR",
+        "IGElement.DEONTIC": "Deontic",
+    }
+
+    tags_tuples = list()
+    for tag in tags:
+        words_with_ids = sorted(tag.words, key=lambda x: x[0])
+        start = id_to_position[words_with_ids[0][0]]
+        stop = id_to_position[words_with_ids[-1][0]] + len(words_with_ids[-1][1])
+        tags_tuples.append(
+            (tag_names[str(tag.tag_name)], start, stop, sentence[start:stop])
+        )
+
+    print(f"{sentence}: {tags}")
+    return (sentence, tags_tuples)
+
+
+def write_mae(output_file, trees_with_tags):
+    sentences_with_tags = [
+        get_sentence_and_tags(tree, tags) for tree, tags in trees_with_tags
+    ]
+
+    with open(output_file, "w", encoding="utf-8") as output:
         output.write('<?xml version="1.0" encoding="UTF-8" ?>\n')
         output.write("<ADICO_test_1>\n")
 
-        sentence = ""
-        id_to_position = dict()
-        for x in tree.get_all_descendants():
-            if x.tag != "PUNCT" and x.id != 1:
-                id_to_position[x.id] = len(sentence) + 1
-                sentence += " " + x.value
-            else:
-                id_to_position[x.id] = len(sentence)
-                sentence += x.value
-        output.write(f"<TEXT><![CDATA[{sentence}]]></TEXT>\n")
+        text = "\n".join([sentence for sentence, _ in sentences_with_tags])
+        output.write(f"<TEXT><![CDATA[{text}]]></TEXT>\n")
 
         output.write("<TAGS>\n")
+
+        offset = 0
         id = 0
-        tag_names = {
-            "IGElement.AIM": "aIm",
-            "IGElement.ATTRIBUTE": "Attribute",
-            "IGElement.OBJECT": "oBject",
-            "IGElement.ACTOR": "aCtor",
-            "IGElement.SEPARATOR": "SEPARATOR",
-            "IGElement.DEONTIC": "Deontic",
-        }
-        for tag in tags:
-            words_with_ids = sorted(tag.words, key=lambda x: x[0])
-            start = id_to_position[words_with_ids[0][0]]
-            stop = id_to_position[words_with_ids[-1][0]] + len(words_with_ids[-1][1])
-            spans = f"{start}~{stop}"
-            output.write(
-                f'<{tag_names[str(tag.tag_name)]} id="x{id}" spans="{spans}" text="{sentence[start:stop]}" />\n'
-            )
-            id += 1
+        for sentence, tags in sentences_with_tags:
+            for tag_name, start, stop, tag_text in tags:
+                spans = f"{offset + start}~{offset + stop}"
+                tag_repr = f'<{tag_name} id="x{id}" spans="{spans}" text="{sentence[start:stop]}" />\n'
+                output.write(tag_repr)
+                id += 1
+            offset += len(sentence) + 1
+
         output.write("</TAGS>\n")
 
         output.write("</ADICO_test_1>")
@@ -69,22 +91,27 @@ def write_output_to_mae(output_file, tree, tags):
 
 @click.command()
 @click.argument("directory", type=click.Path(exists=True))
-def annotate_files(directory):
+@click.argument("output", type=click.Path(exists=False))
+def annotate_files(directory, output):
     executor = IGRulesExecutor()
 
-    for input in listdir(directory):
+    mae_data = list()
+    subset = list(listdir(directory))
+    for input in subset[:6]:
         file_path = pathlib.Path(directory) / pathlib.Path(input)
         if file_path.is_file() and file_path.suffix == ".conllu":
-            with open(file_path, "r+", encoding='utf-8') as input:
+            with open(file_path, "r+", encoding="utf-8") as input:
                 df = pd.read_csv(input, sep="\t", header=None)
                 tree = annotate_df(df)
                 tags = executor.execute(tree)
-                print(tree.show_children_subtrees())
+                # print(tree.show_children_subtrees())
 
-            for tag in tags:
-                print(f"{tag.tag_name}: {tag.words}")
+            # for tag in tags:
+            #     print(f"{tag.tag_name}: {tag.words}")
 
-            write_output_to_mae(file_path.with_suffix(".xml"), tree, tags)
+            mae_data.append((tree, tags))
+
+    write_mae("tmp.xml", mae_data)
 
 
 if __name__ == "__main__":
